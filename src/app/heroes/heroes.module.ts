2b6f97ce4dd7160ad0bb274dde75bb1b63848178
Gonzalo Gestri <gonzalogestri92299@gmail.com>
2023-03-29 20:38:46 -0300

    sidenav

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -6,6 +6,8 @@ import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '../material/material.module';
 
 
 
@@ -20,6 +22,8 @@ import { HeroesRoutingModule } from './heroes-routing.module';
   imports: [
     CommonModule,
     HeroesRoutingModule,
+    FlexLayoutModule,
+    MaterialModule,
   ]
 })
 export class HeroesModule { }
